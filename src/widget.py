fe2d26b617015be104d229befe69ead0e1f6c3f8
Геннадий Филимонов <vfhgjcfl@mail.ru>
2025-11-06 17:58:16 +0300

    добавил widget.py c функциями mask_account_card и get_date

--- src/widget.py
+++ src/widget.py
@@ -16,3 +16,18 @@ def mask_account_card(number_str: str) -> str:
 
 print(mask_account_card("Visa Platinum 7000792289606361"))
 print(mask_account_card("Счет 73654108430135874305"))
+
+
+def get_date(date_str: str) -> str:
+    """функция, которая возвращает строку с датой в формате "ДД.ММ.ГГГГ"."""
+
+    date_one = date_str[:10].split("-")
+    time_id = date_one[2]
+    date_one[2] = date_one[0]
+    date_one[0] = time_id
+    date_two = ".".join(date_one)
+    return date_two
+
+
+
+print(get_date("2024-03-11T02:26:18.671407"))
