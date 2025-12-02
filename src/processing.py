057b42a6d1823dbff0cf55fb5690ee1d070da149
Геннадий Филимонов <vfhgjcfl@mail.ru>
2025-12-02 13:36:08 +0300

    домзадание

--- src/processing.py
+++ src/processing.py
@@ -18,3 +18,27 @@ transactions = [
     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 print(filter_by_state(transactions, "EXECUTED"))
+
+
+from datetime import datetime
+
+
+def sort_by_date(transactions: List[Dict], descending=True) -> List[Dict]:
+    """ "Функция возвращающая новый список, отсортированный по дате"""
+
+    new_transactions = sorted(
+        transactions, key=lambda x: datetime.strptime(x["date"], "%Y-%m-%dT%H:%M:%S.%f"), reverse=descending
+    )
+    return new_transactions
+
+
+print(
+    sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
